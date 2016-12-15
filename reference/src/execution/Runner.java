15faf2e8a31791f011cd85ddf28f5e60dfab2ea6
colugo <colugo.info@gmail.com>
2016-12-16 09:00:27 +1100

    Fix initial drawing bug on OSX

--- reference/src/execution/Runner.java
+++ reference/src/execution/Runner.java
@@ -7,21 +7,22 @@ import viewModel.MainViewModel;
 
 public class Runner {
 
-	
+
 	private static JFrame frame;
-	
+
 	public static void main(String[] args) {
-		
+
 		Font.prepare();
-		
+
 		frame = new JFrame();
 		MainViewModel mainViewModel = new MainViewModel();
-		
+
         frame.setSize(900, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.add(mainViewModel);
-                
+				frame.add(mainViewModel);
+				frame.setVisible(true);
+
+
 	}
 
 
